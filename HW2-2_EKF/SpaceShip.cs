a04c7e49344e376c5d9312ca4f450f292fdcc59b
efeeley <elisabeth.k.feeley@gmail.com>
2017-07-05 15:45:27 -0400

    Add project files.

---
+++ HW2-2_EKF/SpaceShip.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/**************************************
+ * Name: Elisabeth Feeley
+ * Date: 7/7/2012
+ * Assignment: HW2-2_EKF
+ * Resources: N/A
+ * ***********************************/
+
+namespace HW2_2_EKF
+{
+    class SpaceShip
+    {
+        //Attributes
+        private string wingSpan, rocketType, missileType, help;
+
+        //Methods
+
+        public string takeOff()
+        {
+            return getRocketType() +" with wingspan: " + getWingSpan() + " has taken off.";
+        }
+
+        public string land()
+        {
+            return getRocketType() + " with wingspan: " + getWingSpan() + " has landed.";
+        }
+        
+        public string fire()
+        {
+            return getMissileType() + " were fired.";
+        }
+
+
+        public string takeDamage()
+        {
+            return getHelp();
+        }
+        
+        //Gets
+        public string getWingSpan()
+        {
+            return wingSpan;
+        }
+
+        public string getRocketType()
+        {
+            return rocketType;
+        }
+
+        public string getMissileType()
+        {
+            return missileType;
+        }
+
+        public string getHelp()
+        {
+            return help;
+        }
+
+        //Sets
+        public void setWingSpan(string aWingSpan)
+        {
+            wingSpan = aWingSpan;
+        }
+
+        public void setRocketType(string aRocketType)
+        {
+            rocketType = aRocketType;
+        }
+
+        public void setMissileType(string aMissileType)
+        {
+            missileType = aMissileType;
+        }
+
+        public void setHelp(string ahelp)
+        {
+            help = ahelp;
+        }
+    }
+}
---
+++ HW2-2_EKF/TestSpaceShip.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/**************************************
+ * Name: Elisabeth Feeley
+ * Date: 7/7/2012
+ * Assignment: HW2-2_EKF
+ * Resources: N/A
+ * ***********************************/
+
+namespace HW2_2_EKF
+{
+    class TestSpaceShip
+    {
+        static void Main(string[] args)
+        {
+
+            SpaceShip ss1 = new SpaceShip();
+
+            ss1.setRocketType("Starship Enterprise");
+            ss1.setWingSpan("75 Feet");
+            ss1.setMissileType("Blasters");
+            ss1.setHelp("They don't need help, they are that awesome.");
+
+            Console.WriteLine(ss1.takeOff());
+
+            Console.ReadLine();
+
+            Console.WriteLine(ss1.land());
+
+            Console.ReadLine();
+
+            Console.WriteLine(ss1.fire());
+
+            Console.ReadLine();
+
+            Console.WriteLine(ss1.takeDamage());
+
+            Console.ReadLine();
+        }
+    }
+}
